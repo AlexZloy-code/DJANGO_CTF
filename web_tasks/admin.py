0f678b4ef74dd4fe6d5d7b5dc9682d9d094b2641
Saint-Petersburg-Tima <152004758+Saint-Petersburg-Tima@users.noreply.github.com>
2025-01-05 16:06:55 +0300

    more adminka

--- web_tasks/admin.py
+++ web_tasks/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from web_tasks.models import Jobs, UserJobs
 from main.forms import JobsForm
+from django.contrib import admin
+from .models import Jobs
+
+
+def mark_jobs_as_completed(modeladmin, request, queryset):
+    for i in queryset:
+        i.show = 0 if i.show else 1
+        i.save()
+
+
+mark_jobs_as_completed.short_description = "Поменять показ работ"  # Описание действия
+
 
 class JobsAdmin(admin.ModelAdmin):
     form = JobsForm
@@ -14,6 +26,8 @@ class JobsAdmin(admin.ModelAdmin):
     search_fields = ('full_name', 'creator')
 
     list_filter = ('type', 'show')
+
+    actions = [mark_jobs_as_completed]
 
 
 admin.site.register(Jobs, JobsAdmin)
