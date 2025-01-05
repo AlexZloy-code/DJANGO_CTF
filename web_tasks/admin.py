d206e9a22882153a11849d71f67f40516932a077
Saint-Petersburg-Tima <152004758+Saint-Petersburg-Tima@users.noreply.github.com>
2025-01-05 20:23:35 +0300

    last adminka

--- web_tasks/admin.py
+++ web_tasks/admin.py
@@ -31,24 +31,3 @@ class JobsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Jobs, JobsAdmin)
-
-
-@admin.register(UserJobs)
-class UserJobsAdmin(admin.ModelAdmin):
-    list_display = ("user", "job", "date_assigned")
-    search_fields = ("user__username", "job__full_name")
-    list_filter = ("date_assigned",)
-
-
-class CustomAdminSite(admin.AdminSite):
-    site_header = 'My Admin'
-    site_title = 'My Admin Portal'
-    index_title = 'Welcome to My Admin Portal'
-
-    def each_context(self, request):
-        context = super().each_context(request)
-        context['custom_css'] = True
-        return context
-
-
-admin_site = CustomAdminSite(name='custom_admin')
