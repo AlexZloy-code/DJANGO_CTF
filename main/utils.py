7fd816cc5cea91bdfa3e7e8ff10f7f76add5a237
Saint-Petersburg-Tima <152004758+Saint-Petersburg-Tima@users.noreply.github.com>
2025-01-05 15:19:47 +0300

    new

--- main/utils.py
+++ main/utils.py
@@ -6,7 +6,7 @@ def check_flag(user, flag):
         UserJobs.objects.filter(user=user).delete()
     else:
         try:
-            job = Jobs.objects.get(flag=flag)
+            job = Jobs.objects.get(flag=flag, show=True)
             UserJobs.objects.get_or_create(user=user, job=job)
         except Jobs.DoesNotExist:
             pass
