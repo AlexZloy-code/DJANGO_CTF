e4d39082ab0d6997fedb07a19636b841fb0d0cbd
Saint-Petersburg-Tima <152004758+Saint-Petersburg-Tima@users.noreply.github.com>
2025-01-12 21:56:47 +0300

    to docker

--- DJANGO_CTF/DJANGO_CTF/settings.py
+++ DJANGO_CTF/DJANGO_CTF/settings.py
@@ -75,11 +75,9 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = "/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
